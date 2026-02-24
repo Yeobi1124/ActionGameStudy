3d62e9e6c338b0edd68b4abbccd95a9f00b0a162
Yeobi1124 <yeobi1124@gmail.com>
2026-02-25 01:02:22 +0900

    chore : 파일 구조 변경

--- Source/ActionGame/ActionGame.Build.cs
+++ Source/ActionGame/ActionGame.Build.cs
@@ -12,6 +12,8 @@ public class ActionGame : ModuleRules
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		PublicIncludePaths.AddRange(new string[] { "ActionGame" });
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
